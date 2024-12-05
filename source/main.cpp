f8ad3726717d96c4ca077e0867e32609b32630c7
dadev123 <dadev123@github.com>
2024-12-05 13:02:34 +0100

    nothing to see

--- source/main.cpp
+++ source/main.cpp
@@ -188,14 +188,14 @@ void drawMainHook(HakoniwaSequence* curSequence, sead::Viewport* viewport, sead:
         GameMode      gameMode     = GameModeManager::instance()->getGameMode();
         GameModeBase* gameModeBase = GameModeManager::instance()->getMode<GameModeBase>();
 
-        gTextWriter->printf("(R ←)------------ Page %d/%d -------------(R →)\n", pageIndex + 1, maxPages);
+        gTextWriter->printf("(ZR ←)------------ Page %d/%d -------------(ZR →)\n", pageIndex + 1, maxPages);
 
         switch (pageIndex)
         {
         case 0:
             {
                 gTextWriter->printf(
-                    "(ZL ←)----------%s Player %d/%d %s-----------(ZL →)\n\n",
+                    "(R ←)----------%s Player %d/%d %s-----------(R →)\n\n",
                     debugPuppetIndex + 1 < 10 ? "-" : "",
                     debugPuppetIndex + 1,
                     Client::getMaxPlayerCount(),
@@ -590,3 +590,6 @@ void barrierAppearHook(al::LiveActor* thisPtr, const char* actionName){
 bool checkAssistMode(GameDataHolderAccessor accessor){
     return !rs::isKidsMode(accessor.mData);
 }
+
+
+// hi
