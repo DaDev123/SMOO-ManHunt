37fbef583788e2515d5bbafca563394f5537bdc4
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-01-29 21:40:34 +0100

    Update HideAndSeekMode.cpp

--- source/server/hns/HideAndSeekMode.cpp
+++ source/server/hns/HideAndSeekMode.cpp
@@ -127,13 +127,13 @@ void HideAndSeekMode::begin() {
 
 if (mInfo->mIsPlayerIt) {
             PlayerHitPointData* hit = mCurScene->mHolder.mData->mGameDataFile->getPlayerHitPointData();
-            hit->mCurrentHit = hit->recoverMax();
+            hit->mCurrentHit = hit->getMaxCurrentWithoutItem();
             hit->mIsKidsMode = true;
             
             pause();
     } else {
           PlayerHitPointData* hit = mCurScene->mHolder.mData->mGameDataFile->getPlayerHitPointData();
-            hit->mCurrentHit = hit->recoverMax();
+            hit->mCurrentHit = hit->GetMaxCurrentWithoutItem();
             hit->mIsKidsMode = false;
             pause();
         }
