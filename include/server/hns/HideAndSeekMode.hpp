9cc27c618e114f363e068573eb2f2c838873e6c7
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-01-30 18:14:13 +0100

    Update HideAndSeekMode.hpp

--- include/server/hns/HideAndSeekMode.hpp
+++ include/server/hns/HideAndSeekMode.hpp
@@ -65,6 +65,10 @@ class HideAndSeekMode : public GameModeBase {
 
         void setCameraTicket(al::CameraTicket* ticket) { mTicket = ticket; }
 
+        HideAndSeekInfo* getInfo() const {
+        return mInfo;
+        }
+
     private:
         float mInvulnTime = 0.0f;
         GameModeTimer* mModeTimer = nullptr;
